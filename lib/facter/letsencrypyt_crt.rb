513aca810cd5243b30eb581a978bb8d3af5121d0
Mikhail Konyakhin <m.konyakhin@city-mobil.ru>
2018-03-13 14:31:28 +0300

    Add new facters and enable support letsencrypt server not only at puppet master.

--- lib/facter/letsencrypyt_crt.rb
+++ lib/facter/letsencrypyt_crt.rb
@@ -8,3 +8,33 @@ Facter.add(:letsencrypt_crts) do
     crt_domains.join(',') if crt_domains
   end
 end
+
+Facter.add(:letsencrypt_crts_content) do
+  setcode do
+    crt = {}
+    crt_domains.each do |crt_domain|
+      crt[crt_domain] = File.read("/opt/letsencrypt/requests/#{crt_domain}/#{crt_domain}.crt")
+    end
+    crt
+  end
+end
+
+Facter.add(:letsencrypt_crts_ocsp_content) do
+  setcode do
+    ocsp = {}
+    crt_domains.each do |crt_domain|
+      ocsp[crt_domain] = File.read("/opt/letsencrypt/requests/#{crt_domain}/#{crt_domain}.crt.ocsp")
+    end
+    ocsp
+  end
+end
+
+Facter.add(:letsencrypt_crts_ca_content) do
+  setcode do
+    ca = {}
+    crt_domains.each do |crt_domain|
+      ca[crt_domain] = File.read("/opt/letsencrypt/requests/#{crt_domain}/#{crt_domain}_ca.pem")
+    end
+    ca
+  end
+end
